d7f9a9227b7e437b3da05d1826b0bf0ea98917ba
Hrvoje Bernardić <hbernardic@gmail.com>
2016-06-13 22:00:28 +0200

    vscode typings test

--- client/app/components/app.component.ts
+++ client/app/components/app.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: ` <h1>My First Angular 2 App</h1>
+                <p>Dodo je tu traallalala!! {{title}}</p>
+    `
 })
-export class AppComponent { }
+export class AppComponent { 
+    title: string;
+
+    constructor() {
+        this.title = "ovo je naslov";
+        
+    }
+}
